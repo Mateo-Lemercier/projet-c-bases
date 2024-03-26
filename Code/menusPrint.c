aa955755cb2026ee90c63e0d292eb51f38ee382d
Mateo-Lemercier <mlemercier@gaming.tech>
2024-03-26 14:24:13 +0100

    attempt-to-solve-bug

---
+++ Code/menusPrint.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <varTypes.h>
+
+
+
+
+
+void PrintLoop( const char *const sStringToPrint, const uint2 iCount )
+{
+    for ( uint2 i = 0; i < iCount; i++ )
+        printf( "%s", sStringToPrint );
+}
+
+void PrintLoopWith( const char *const sStringBefore, const char *const sStringToPrint, const char *const sStringAfter, const uint2 iCount )
+{
+    printf( "%s", sStringBefore );
+    
+    for ( uint2 i = 0; i < iCount; i++ )
+        printf( "%s", sStringToPrint );
+    
+    printf( "%s", sStringAfter );
+}
+
+
+
+
+
+void PrintTitle( const char *const title, uint1 iSize )
+{
+    uint1 spaceCount;
+    system( "cls" );
+    
+    PrintLoopWith( "\033[0;47m", " ", "\n", 119 );
+    
+    for (uint1 terminalRow = 0 ; terminalRow < 2 ; terminalRow++)
+        PrintLoopWith( " \033[0m", " ", "\033[0;47m \n", 117 );
+    
+    for (uint1 characterRow = 0 ; characterRow < 6 ; characterRow++)
+    {
+        printf( " \033[0m" );
+        spaceCount = (117 - 9*iSize)/2;
+        for (uint1 space = 0 ; space < 117 - 9*iSize - spaceCount ; space++)
+            printf( " " );
+        
+        PrintBigText( title, characterRow );
+        
+        for (uint1 space = 0 ; space < spaceCount ; space++)
+            printf( " " );
+        printf( "\033[0;47m \n" );
+    }
+    
+    for (uint1 terminalRow = 8 ; terminalRow < 10 ; terminalRow++)
+        PrintLoopWith( " \033[0m", " ", "\033[0;47m \n", 117 );
+    
+    PrintLoopWith( "", " ", "\n", 119 );
+}
+
+
+int main()
+{
+    PrintTitle( "Minesweeper", 12 );
+    return 0;
+}
---
+++ Code/varTypes.h
@@ -0,0 +1,17 @@
+#ifndef __varTypes_h__
+#define __varTypes_h__
+
+typedef int int4;
+typedef unsigned int uint4;
+
+typedef short int2;
+typedef unsigned short uint2;
+
+typedef char int1;
+typedef unsigned char uint1;
+
+typedef char boolean;
+#define true 1
+#define false 0
+
+#endif
