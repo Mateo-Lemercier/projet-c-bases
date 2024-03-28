77792c190cafc631d459bf8bb3eb4c280238b462
Mateo-Lemercier <mlemercier@gaming.tech>
2024-03-28 10:55:34 +0100

    prints-updated

---
+++ cvarTypes.h
@@ -0,0 +1,17 @@
+#ifndef __cvarTypes_h__
+#define __cvarTypes_h__
+
+typedef int int4;
+typedef unsigned int uint4;
+
+typedef short int2;
+typedef unsigned short uint2;
+
+typedef char int1;
+typedef unsigned char uint1;
+
+typedef char bool;
+#define true 1
+#define false 0
+
+#endif
--- menusPrint.c
+++ menusPrint.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "varTypes.h"
+#include "cvarTypes.h"
 #include "bigCharacters.c"
+
+typedef struct Vector2
+{
+    uint1 row;
+    uint1 col;
+} Vector2;
+
+typedef struct Menu
+{
+    char table[ 15 ][ 119 ];
+    uint1 iSelected;
+    Vector2 *canBeSelected;
+    uint1 canBeSelectedCount;
+} Menu;
 
 
 
@@ -27,8 +41,16 @@ void PrintLoopWith( const char *const sStringBefore, const char *const sStringTo
 
 
 
-void PrintTitle( const char *const title, uint1 iSize )
+void PrintTitle( const char *const title )
 {
+    uint1 iSize = 0;
+    for ( uint1 i = 0 ; title[ i ] != '\0' ; i++ )
+    {
+        iSize++;
+        if ( title[ i ] == 'm' || title[ i ] == 'w' )
+            iSize++;
+    };
+    
     uint1 spaceCount;
     system( "cls" );
     
@@ -57,10 +79,78 @@ void PrintTitle( const char *const title, uint1 iSize )
     PrintLoopWith( "", " ", "\n", 120 );
 }
 
+void PrintMenu( Menu *pMenu )
+{
+    for ( uint1 i = 0 ; i < 15 ; i++)
+    {
+        printf( " \033[0m" );
+        for ( uint1 j = 0 ; j < 119 ; j++ )
+        {
+            if ( pMenu->canBeSelected[ pMenu->iSelected ].row == i && pMenu->canBeSelected[ pMenu->iSelected ].col == j )
+                printf( "\033[4;36m" );
+            
+            printf( "%c", pMenu->table[ i ][ j ] );
+        }
+        printf( "\033[0;47m \n" );
+    }
+    
+    PrintLoopWith( "\033[0;47m", " ", "\n", 120 );
+}
+
+
+
+
+
+void PlaceText( Menu *pMenu, char *sText, uint1 iRow, uint1 iColumn, bool bCanBeSelected )
+{
+    for ( uint1 i = 0 ; sText[ i ] != '\0' ; i++ )
+        pMenu->table[ iRow ][ iColumn + i ] = sText[ i ];
+    
+    if ( bCanBeSelected )
+    {
+        pMenu->canBeSelectedCount++;
+        pMenu->canBeSelected = ( Vector2* ) realloc( pMenu->canBeSelected, sizeof( Vector2 ) * pMenu->canBeSelectedCount );
+        pMenu->canBeSelected[ pMenu->canBeSelectedCount - 1 ].row = iRow;
+        pMenu->canBeSelected[ pMenu->canBeSelectedCount - 1 ].col = iColumn;
+    }
+}
+
+
+
+
+
+void OpenMenu( const char *const sTitle )
+{
+    Menu menu;
+    menu.iSelected = 0;
+    menu.canBeSelected = ( Vector2* ) malloc( sizeof( Vector2 ) );
+    menu.canBeSelectedCount = 0;
+    for ( uint1 i = 0 ; i < 15 ; i++ )
+    {
+        for ( uint1 j = 0 ; j < 118 ; j++ )
+            menu.table[ i ][ j ] = ' ';
+        menu.table[ i ][ 118 ] = '\0';
+    }
+    
+    PlaceText( &menu, "Start", 8, 12, true );
+    PlaceText( &menu, "Noaleplusbo", 2, 74, true );
+    
+    while ( true )
+    {
+        PrintTitle( sTitle );
+        PrintMenu( &menu );
+        break;
+    }
+}
+
+
+
+
 
 int main()
 {
-    PrintTitle( "Minesweeper", 12 );
+    // PrintTitle( "Minesweeper" );
+    OpenMenu( "Minesweeper" );
     while( true );
     return 0;
 }
